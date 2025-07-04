d685a787d947e849a3ea0ec833351991cf39258e
jhwu16 <64880558+jhwu16@users.noreply.github.com>
2025-07-04 10:35:39 +0800

    Add files via upload

---
+++ CLS_audio_only/audio_dataset_subj.py
@@ -0,0 +1,81 @@
+
+# encoding: utf-8
+import os
+import glob
+import random
+import numpy as np
+import librosa
+import torch
+from python_speech_features import mfcc
+# from lpctorch import LPCCoefficients
+from matplotlib.pyplot import figure
+from matplotlib import pyplot as plt
+
+
+def add_noise(signal):
+    SNR = 0
+    noise = np.random.randn(signal.shape[0]) # 高斯白噪声
+    noise = noise - np.mean(noise)
+    signal_power = np.linalg.norm(signal) ** 2 / signal.size
+    noise_variance = signal_power / np.power(10, (SNR / 10))
+    noise = (np.sqrt(noise_variance) / np.std(noise)) * noise
+    signal_noise = noise + signal
+    return signal_noise
+
+
+def get_MFSC(fs, x):
+    x = np.array(x).astype(float)
+    n_mels = 64
+    norm_x = x[:]
+    tmp = librosa.feature.melspectrogram(y=norm_x, sr=fs, n_mels=n_mels, n_fft=1500, hop_length=735)
+    mfsc_x = librosa.power_to_db(tmp).T
+    return mfsc_x  # (60,64)
+
+
+class MyDataset():
+    def build_file_list(self, subj):
+        List = []
+
+        audio_subject_list = np.load('/ai/benchmark/fusion_baseline_231205_new/audio_only/audio_subject.npy')
+        audio_subject_list = audio_subject_list.tolist()
+
+        audio_dataset = audio_subject_list[subj]
+        sessions = os.listdir(audio_dataset)
+        for session in sessions:
+            samples = os.listdir(audio_dataset + '/' + str(session))
+            for sample in samples:
+                label = sample.split('.')[0]
+                audiopath = audio_dataset + '/' + session + '/' + str(label) + '.wav'
+                entry = (label, audiopath)
+                List.append(entry)
+
+        random.shuffle(List)
+        return List
+
+        # completeList : 列表保存 ( label ，文件绝对路径 )
+
+    def __init__(self, subject):
+        # file_list : 文件列表
+        self.file_list = self.build_file_list(subject)
+
+        # 打印该类型数据集（ 训练 or 测试 ）的样本总数
+        print('Total num of samples: ', len(self.file_list))
+        
+
+    def __getitem__(self, idx):
+
+        audio, fs = librosa.load(self.file_list[idx][1])
+        audio = add_noise(audio)
+        # audio = mfcc(audio)
+        audio = get_MFSC(fs,audio)
+        # audio = LPCCoefficients(audio)
+        # print('sampling rate:', fs) fs = 22050
+
+
+        label = int(self.file_list[idx][0])
+        audio = torch.FloatTensor(audio[np.newaxis, :])
+
+        return audio, label
+
+    def __len__(self):
+        return len(self.file_list)
