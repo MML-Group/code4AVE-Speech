06dbc13702cfa6427b333795fd4a125ea735c9ec
jhwu16 <64880558+jhwu16@users.noreply.github.com>
2025-07-15 19:48:55 +0800

    Update concat_model_trans.py

--- CLS_fusion/concat_model_trans.py
+++ CLS_fusion/concat_model_trans.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 
-class TransformerGate(nn.Module):
+class TransformerConcat(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes, every_frame=True):
         super(TransformerGate, self).__init__()
         self.hidden_size = hidden_size
@@ -20,20 +20,12 @@ class TransformerGate(nn.Module):
         self.transformerEncoder = nn.TransformerEncoder(encoder_layer, num_layers=1)
 
     def forward(self, x):
-        # h0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size))
-        # Forward propagate RNN
-        # out, hn = self.gru(x, h0)
-        # w = hn[-1, :, :]
-        # w = w.unsqueeze(0)
-        # batch, 101, 184
         x = self.transformerEncoder(x)
-        # x = torch.mean(x, dim=2)
-        # x = self.fc(x)
         return x
 
 
 
 def TRANSFORMERConcat(mode, inputDim=2048, hiddenDim=2048, nLayers=2, nClasses=500, frameLen=29, every_frame=True):
-    model = TransformerGate(inputDim, hiddenDim, nLayers, nClasses, every_frame)
+    model = TransformerConcat(inputDim, hiddenDim, nLayers, nClasses, every_frame)
     print('\n'+mode+' model has been built')
     return model
